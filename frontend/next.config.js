0d9bb5933a120c4cf30912b77e071d19932184bd
congo musah adama <musahcongoadama@gmail.com>
2025-09-15 15:33:42 +0000

    u

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:4000',
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://socialauth-1tz8.onrender.com',
   },
 }
 
